45295b33eed8bd96983b4c4824f875931a69e220
Fidel Leal <fidelxleal@gmail.com>
2022-11-11 12:53:04 -0600

    add solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = arguments
 
+    if (arr === undefined) return arr;
+
+    if (args.length === 1) return arr;
+
+    for (let i = 1; i < args.length; i++) {
+        let index = arr.indexOf(args[i]);
+        if (index >= 0) {
+            arr.splice(index, 1);
+        }
+
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
