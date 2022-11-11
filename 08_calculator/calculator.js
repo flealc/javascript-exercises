9f40206328ac995c17f604cab57fb1f4e721d5f5
Fidel Leal <fidelxleal@gmail.com>
2022-11-11 15:34:14 -0600

    add solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+	return arr.reduce((val1,val2) => add(val1,val2), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((val1,val2) => val1 * val2, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+    return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    return (x === 0) ? 1 : x * factorial(x - 1);
 };
 
 // Do not edit below this line
