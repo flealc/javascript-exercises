a290cdccac7a990465099744d15f0621f99d3123
Fidel Leal <fidelxleal@gmail.com>
2022-11-11 14:06:07 -0600

    add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    if (typeof a != 'number' || typeof b != 'number') {
+        return 'ERROR';
+    }
+
+    return (Math.abs(b - a) + 1) * (a + b) / 2;
 
 };
 
