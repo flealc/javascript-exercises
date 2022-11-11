4efd548c5074a85e0734dc915b4b93502795957e
Fidel Leal <fidelxleal@gmail.com>
2022-11-11 11:33:14 -0600

    add solution to exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let str = '';
 
+    for (let i = 0; i < num; i ++) {
+        str = str.concat(string);
+    }
+
+    return str;
 };
 
 // Do not edit below this line
