a57fc2afd5b1ef3a24f6cd39523802e60241a70b
Fidel Leal <fidelxleal@gmail.com>
2022-11-12 16:31:57 -0600

    add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(position, number = 1, previous = 0) {
+    position = parseInt(position);
+    if (position <= 0) return "OOPS"
+    if (position === 1) return number;
+    return fibonacci(position - 1, number + previous, number)
 };
 
 // Do not edit below this line
