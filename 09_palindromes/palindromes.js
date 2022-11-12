c27bb58ba004f8a8b928082ff66eb9d1da157c8e
Fidel Leal <fidelxleal@gmail.com>
2022-11-12 07:17:19 -0600

    add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+    string = string.replace(/[^\w\']|_/g,'').toLowerCase()
+    let rev = string.split('').reverse().join('')
+
+    return string === rev ? true : false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
