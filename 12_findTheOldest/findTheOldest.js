dce183646960307976f69a20d135e846e59e181e
Fidel Leal <fidelxleal@gmail.com>
2022-11-17 11:01:07 -0600

    add solution to problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let person = undefined;
+    let age = 0;
 
+    function find(currentPerson) {
+
+        let currentAge = undefined;
+        if (currentPerson.yearOfDeath === undefined) {
+            let now = new Date(Date()).getFullYear();
+            currentAge = now - currentPerson.yearOfBirth;
+            if (currentAge > age) {
+                age = currentAge;
+                person = currentPerson;
+            }
+        }
+        else {
+            currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+            if (currentAge > age) {
+                age = currentAge;
+                person = currentPerson;
+            }
+        }
+
+    }
+    people.forEach(currentPerson => find(currentPerson));
+    return person;
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
